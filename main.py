a7438279bdf3e69d4fbc79307aa56d6cac80922a
Rashel Sarker <rashelislam8070@gmail.com>
2025-10-08 14:53:59 +0100

    Fully working pdf merger

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Hello World")
-print("PDF Merger")
+from PyPDF2 import PdfMerger
+import sys
+import os
+
+merger = PdfMerger()
+
+
+for filename in os.listdir(os.curdir):
+    if filename.endswith(".pdf"):
+        merger.append(filename)
+
+merger.write("Combined.pdf")
+merger.close()
+print("pdf merge completed")
